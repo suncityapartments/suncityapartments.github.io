1fcacc3db9cec4f7a73d55e2da8f0daa62fba2ad
Anant Anand Gupta <anantanandgupta@gmail.com>
2023-09-29 06:41:59 +0530

    bug fixes and navigation changes

--- applications/water-billing/src/app/water/water-routing.module.ts
+++ applications/water-billing/src/app/water/water-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WaterComponent } from './water.component';
+import { MeteredComponent } from './pages/metered/metered.component';
 
-const routes: Routes = [{ path: '', component: WaterComponent }];
+const routes: Routes = [{ 
+  path: '', 
+  component: WaterComponent,
+  children: [
+    {
+      path: '',
+      redirectTo: 'metered',
+      pathMatch: 'full',
+    },
+    {
+      path: 'metered',
+      component: MeteredComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- applications/water-billing/src/app/water/water.module.ts
+++ applications/water-billing/src/app/water/water.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { WaterRoutingModule } from './water-routing.module';
 import { WaterComponent } from './water.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MeteredComponent } from './pages/metered/metered.component';
 
 
 @NgModule({
   declarations: [
-    WaterComponent
+    WaterComponent,
+    MeteredComponent
   ],
   imports: [
     CommonModule,
